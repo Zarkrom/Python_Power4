83dfc79b078a34ff11b7dc9be2162618e6e95e85
tS6wG#!#wABQ632 <erwann-marobin@hotmail.fr>
2022-11-29 16:43:37 +0100

    2.1.5

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import asyncio
 import websockets
+import json
+from power4 import PLAYER1, PLAYER2
 
 """ Fichier principal du serveur
     Contient une boucle infinie d'écoute : main() sur localhost:8001
@@ -9,12 +11,26 @@ import websockets
 """
 
 async def handler(websocket):
+    redTurn = True
     while True:
-        try:
-            message = await websocket.recv()
-        except websockets.ConnectionClosedOK:
-            break
-        print(message)
+        # Lecture du message envoyé par le navigateur
+        async for message in websocket:
+            # A chaque tour on change de joueur
+            player = PLAYER1 if redTurn else PLAYER2
+            redTurn = not redTurn
+            event = json.loads(message)
+            assert event["type"] == "play"
+            column = event["column"]
+            # On forge un objet JSON event
+            event = {
+                "type": "play",
+                "player": player,
+                "column": column,
+                "row": 0, # Pour l'instant on ne sait jouer que sur la ligne du bas
+            }
+            # On l'envoi au navigateur pour affichage
+            await websocket.send(json.dumps(event))
+
 
 async def main():
     async with websockets.serve(handler, "", 8001):
